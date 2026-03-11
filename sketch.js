6b4d22e00b2ab486b1046dba160ea6ff2749b519
mavih2115 <teodoro.batista@escola.pr.gov.br>
2026-03-11 15:19:31 -0300

    sket123

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+let terra;
+
+function preload(){
+    terra = loadImage('terra.jpg')
+}
 function setup() { 
     creatCanvas(1920,1080, WEBGL);//plano 3D
-    terra=loadImage('terrajpl)
-}
+   }
 
 function draw() { 
     background(205, 102, 94);//cor de fundo
     rotatey(millis()/1000);//rotação da terra 
-    TextTrackCue(terra);//textura da terra, imagem.
-    Sphere(80, 100);//esfera 3D
+    textTrackCue(terra);//textura da terra, imagem.
+    sphere(80, 100);//esfera 3D
 }
